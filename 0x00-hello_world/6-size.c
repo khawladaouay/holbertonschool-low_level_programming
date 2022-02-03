bd7ce6e3fd282ee539dfe32996b2d3284a4dfbcb
khawladaouay <daouaykhawla782@gmail.com>
2022-02-03 15:05:19 +0100

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    int intType;
-    float floatType;
-    long int longinttype;
-    long long int doubellongint;
-    char charType;
+int intType;
+float floatType;
+long int longinttype;
+long long int doubellongint;
+char charType;
 
-    printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-    printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(longinttype));
-    printf("Size of a long long int: %zu byte(s)\n",sizeof(doubellongint));
-    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-    return (0);
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longinttype));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(doubellongint));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
 }
