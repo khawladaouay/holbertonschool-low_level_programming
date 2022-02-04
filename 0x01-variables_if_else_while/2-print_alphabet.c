66afb5f71a7bbcdddabad37d278d5f275f2dd801
khawladaouay <daouaykhawla782@gmail.com>
2022-02-04 14:40:23 +0100

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints if number is positive
+ *Return: 0
+*/
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; ++c)
+	{
+	putchar(c);
+	}
+	return (0);
+}
