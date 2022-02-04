3962a882bb1fbcaef42abfcddaed84cb1e316ead
khawladaouay <daouaykhawla782@gmail.com>
2022-02-04 18:41:41 +0100

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,12 @@ int main(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-	putchar(n + '0');
-	if (n <= 8)
-	putchar(',');
-	putchar(' ');
+		putchar(n + '0');
+		if (n <= 8)
+		{
+		putchar(',');
+		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
