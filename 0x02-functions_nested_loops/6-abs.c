0a607cf8cb3dd010faa466b6564c23f5b2dc3fcb
khawladaouay <daouaykhawla782@gmail.com>
2022-02-08 15:51:56 +0100

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - writes the character c to stdout
+ * *@n: input
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _abs(int n)
+{
+	int n;
+
+	if (n < 0)
+	return (-n);
+else
+return (n);
+}
