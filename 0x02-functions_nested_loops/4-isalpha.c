bceeec7e11775b80bfacdde155428e79650d8427
khawladaouay <daouaykhawla782@gmail.com>
2022-02-08 14:40:22 +0100

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - writes the character c to stdout
+ * @c: input
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
