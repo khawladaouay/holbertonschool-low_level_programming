dd5408551f25bf850020c41757610eab13ae9570
khawladaouay <daouaykhawla782@gmail.com>
2022-02-08 13:10:14 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0; i <= 10 ; i++)
+	{char c;
+		for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	}
+	_putchar('\n');
+}
