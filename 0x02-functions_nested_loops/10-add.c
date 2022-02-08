8f815322c30b795b372f1dd274aa387bf44d23ea
khawladaouay <daouaykhawla782@gmail.com>
2022-02-08 22:30:19 +0100

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - writes the character c to stdout
+ *@a , @b: input
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{ int sum;
+	sum = a+b;
+	return (sum);
+}
