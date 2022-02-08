5890cb2f66dc7f0788283cfbd414cd322f4a1f43
khawladaouay <daouaykhawla782@gmail.com>
2022-02-08 23:33:39 +0100

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (i = n ; i <= 98; i++)
 		{
@@ -21,6 +21,10 @@ void print_to_98(int n)
 				_putchar(' ');
 			}
 		}
+	}
+	else if (n == 98)
+	{
+		_putchar('0' + n);
 	}
 	else
 	{
