9ec54341df5d76e0d91488ae917ae1ce597ca832
khawladaouay <daouaykhawla782@gmail.com>
2022-02-08 14:32:46 +0100

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include "main.h"
 /**
- * print_alphabet_x10 - writes the character c to stdout
- *
+ * _islower - writes the character c to stdout
+ * @c: input
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _islower(int c)
 {
-	int c;
 	if(c >= 'a' && c <= 'z')
 	{
 		return (1);
