cd74c065e800765dc61df7c74ed522cefeba3a1b
khawladaouay <daouaykhawla782@gmail.com>
2022-02-08 16:10:00 +0100

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - writes the character c to stdout
+ * *@n: input
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_last_digit(int n)
+{
+	n= n % 10;
+}
