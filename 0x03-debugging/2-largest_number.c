0787e61734f436275813fa949d6adb2a22504463
khawladaouay <daouaykhawla782@gmail.com>
2022-02-10 10:44:08 +0100

    task 2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a == 0)
+	{
+		largest = a;
+	}
+	else if (b == 0)
+	{
+		largest = b;
+	}
+	else if (c == 0)
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
