5d2bd31147852ba958830f29c9984db9ee8d3503
khawladaouay <daouaykhawla782@gmail.com>
2022-02-10 12:16:39 +0100

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * int _isupper- check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{int i;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
