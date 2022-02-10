7c3c52c58c4f2b8e0193fe274c0e0e7555d148cf
khawladaouay <daouaykhawla782@gmail.com>
2022-02-10 12:21:48 +0100

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,19 +2,19 @@
 
 
 /**
- * int _isupper- check the code.
- *
+ * _isupper- check the code.
+ *@c:input
  * Return: Always 0.
  */
 int _isupper(int c)
 {int i;
 
-	if (c >= 'a' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
