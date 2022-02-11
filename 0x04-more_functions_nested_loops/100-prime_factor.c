b4afb98d21c12e2076646adbcdf99b0e7850dcf0
khawladaouay <daouaykhawla782@gmail.com>
2022-02-11 11:31:18 +0100

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,21 +2,23 @@
 #include <math.h>
 /**
  * main - check the code
+ *n 612852475143
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	long f, n, lg;
 
 	n = 612852475143;
-	for (f = 0; f < sqrt(n); f++)
+	for (f = 2; f < sqrt(n); f++)
 	{
-		if (n % f == 0)
+		if ((n % f) == 0)
 		{
 			n = f;
-			n /= f;
-			d = 1;
+			n = n / f;
+			f = 1;
 		}
 	}
 	ln = n;
