810da4d14598044de0bd9ddee02529cca8563756
khawladaouay <daouaykhawla782@gmail.com>
2022-02-14 18:46:34 +0100

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,12 @@ void puts2(char *str)
 {
 	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 		{
 		_putchar(str[i]);
 		}
-		i++;
 	}
 	_putchar('\n');
 }
