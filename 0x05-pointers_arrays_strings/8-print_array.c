be9b38c4f68043f4d33148a2a8ad027d6ebc838e
khawladaouay <daouaykhawla782@gmail.com>
2022-02-14 19:23:34 +0100

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_array- prints  a string
+ * @a: input
+ * @n: input
+ * Return: void
+ */
+void print_array(int *a, int n)
+{int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	_putchar('\n');
+}
