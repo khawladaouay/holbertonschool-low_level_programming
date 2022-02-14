41923dc127d08830ef584026fb23d4de7d15b57c
khawladaouay <daouaykhawla782@gmail.com>
2022-02-14 14:43:23 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_rev- prints  a string
+ * @s: input
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, b;
+	int a = 0; 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		a = a + 1;
+	}
+	for (b = a-1; b >= 0 ; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
