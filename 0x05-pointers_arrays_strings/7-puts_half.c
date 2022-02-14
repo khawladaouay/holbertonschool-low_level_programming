b8816ca208c8a901281674ffc59a155880f60134
khawladaouay <daouaykhawla782@gmail.com>
2022-02-14 19:09:55 +0100

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,10 +9,8 @@ void puts_half(char *str)
 	int i, j;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
-		j = (i + 1) / 2;
-	}
-	for (j = 0; str[j] != '\0'; j++)
+	j = (i + 1) / 2;
+	for (str[j] != '\0'; j++)
 	{
 		_putchar(str[i]);
 	}
