73788bfb19ef42f65e090f3a9a5d7d7131c6012f
khawladaouay <daouaykhawla782@gmail.com>
2022-02-14 18:07:15 +0100

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *rev_string- prints  a string
  * @s: input
@@ -6,16 +7,17 @@
  */
 void rev_string(char *s)
 {
-	int i, b;
+	int i, b, t;
 	int a = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		a = a + 1;
 	}
-	for (b = a - 1; b >= 0 ; b--)
+	for (b = 0; b < a / 2; b++)
 	{
-		_putchar(s[b]);
+		t = s[b];
+		s[b] = s[a - b - 1];
+		s[a - b - 1] = t;
 	}
-	_putchar('\n');
 }
