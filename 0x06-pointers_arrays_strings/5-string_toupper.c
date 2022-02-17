e2ad83fc09f4b12a2c93a37c47d6c6d9b7e3c7d4
khawladaouay <daouaykhawla782@gmail.com>
2022-02-17 10:20:37 +0100

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,10 +8,10 @@
 char *string_toupper(char *s)
 {	int c;
 
-	 for (c = 0;  s[c] != '\0'; c++)
+	for (c = 0;  s[c] != '\0'; c++)
 	{
 		if (s[c] >= 'a' && s[c] <= 'z')
 			s[c] = s[c] - 32;
 	}
-	return(s);
+	return (s);
 }
