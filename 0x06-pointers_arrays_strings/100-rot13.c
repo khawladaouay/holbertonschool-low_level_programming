acc489861628018a0251157813b0115e2ec78b69
khawladaouay <daouaykhawla782@gmail.com>
2022-02-17 19:17:31 +0100

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  **rot13- function that encodes a string using rot13
  *@s: input
@@ -7,15 +6,16 @@
  */
 char *rot13(char *s)
 {	int i, j;
-char a[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
-char b[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
 		if (s[i] == a[j])
 	{
 		s[i] = b[j];
+		break;
 	}
 	}
-	printf("%c", s);
+	return(s);
 }
