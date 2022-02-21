673d36c61a5a2bbdd70ca758cd1928d689a37067
khawladaouay <daouaykhawla782@gmail.com>
2022-02-21 13:11:54 +0100

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *csrc = (char *)src;
 	char *cdest = (char *)dest;
+	unsigned int i;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
+	{
 		cdest[i] = csrc[i];
+	}
+	return (cdest);
 }
