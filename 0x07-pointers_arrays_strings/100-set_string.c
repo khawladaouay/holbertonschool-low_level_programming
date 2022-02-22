e15a305804bce2e7eb484517bc946e03a1cada57
khawladaouay <daouaykhawla782@gmail.com>
2022-02-22 15:29:31 +0100

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ ** set_string- a function that copies memory area
+ * @a: input
+ * @size: input
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
