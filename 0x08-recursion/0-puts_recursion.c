9b21098aa8e7d25fddff998d1e93245a45a85e4d
khawladaouay <daouaykhawla782@gmail.com>
2022-02-23 10:33:52 +0100

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ **_puts_recursion- prints  a string
+ * @s: input
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	_putchar('\n');
+	else
+	{	_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
