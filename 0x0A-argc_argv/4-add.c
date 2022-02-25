d1be80d4a6abdb3b936a8597615d9ea53b40e29f
khawladaouay <daouaykhawla782@gmail.com>
2022-02-25 16:46:43 +0100

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - check the code
+ * @argc: number of arguments aupplied to program
+ * @argv: array that contains the arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int result, i;
+
+	result = 0;
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	}
+	return (0);
+}
