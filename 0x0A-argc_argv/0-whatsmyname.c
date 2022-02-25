cbc931bee7aa8df64ce4e5d5758cd89216f560b8
khawladaouay <daouaykhawla782@gmail.com>
2022-02-25 12:21:43 +0100

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - a program that prints its name
  * @argc: input
  *@argv: input
- * return: int
+ * Return: always 0 (success)
  */
 int main(int argc __attribute__ ((unused)), char *argv[])
 {
