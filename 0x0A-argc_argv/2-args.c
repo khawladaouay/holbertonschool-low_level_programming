45bcf440814dfbeabb58991daadaa8ff56a6d4ef
khawladaouay <daouaykhawla782@gmail.com>
2022-02-25 12:08:01 +0100

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-a program that prints all arguments it receives
+ * @argc: input
+ *@argv: input
+ */
+int main(int argc, char *argv[])
+{int i;
+
+for (i = 0; i < argc; i++)
+{
+	printf("%s\n", argv[i]);
+}
+	return (0);
+}
