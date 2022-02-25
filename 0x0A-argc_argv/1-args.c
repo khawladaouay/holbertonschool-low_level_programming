25c086abfbd64360b4397c7dba2a9844f890fbf0
khawladaouay <daouaykhawla782@gmail.com>
2022-02-25 12:20:51 +0100

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
  * main- a program that prints the number of arguments passed into it
  * @argc: input
  *@argv: input
- * return: int
+ * Return: always 0 (success)
  */
 int main(int argc, char *argv[] __attribute__ ((unused)))
 {
