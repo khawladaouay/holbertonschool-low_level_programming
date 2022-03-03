b7cf4dce34fa32ce08121651fe425541f0ef910d
khawladaouay <daouaykhawla782@gmail.com>
2022-03-03 12:11:43 +0100

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * string_nconcat -  concatenates two strings.
  * @s1 : input
@@ -11,34 +11,24 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {	char *s3;
-	unsigned int i = 0, j = 0, len1, len2;
+	unsigned int i = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
-	s3 = malloc(sizeof(char) * (len1 + len2) + 1);
-		if (s3 == NULL)
-		{
+	if (n > strlen(s2))
+		n = strlen(s2);
+	s3 = malloc(sizeof(char) * (strlen(s1) + n + 1));
+	if (s3 == NULL)
 		return (NULL);
-		}
-		while (*s1 != '\0')
-		{
-			s3[i] = *s1;
-			s1++;
-			i++;
-		}
-
-		for (j = 0; s2[j] != '\0' && j <= n; j++)
-		{
-			s3[i] = s2[j];
-			i++;
-		}
-		s3[i] = '\0';
-
+	for (i = 0; i < (strlen(s1) + n); i++)
+	{
+		if (i < strlen(s1))
+			s3[i] = s1[i];
+		else
+			s3[i] = s2[i - strlen(s1)];
+	}
+	s3[i] = '\0';
 	return (s3);
 }
