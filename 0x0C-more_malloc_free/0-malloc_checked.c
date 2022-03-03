b3e8b86b8f1057d88b4132cdc7160711d718ebda
khawladaouay <daouaykhawla782@gmail.com>
2022-03-03 10:55:45 +0100

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory 
+ * @b:input;
+ * Return: none
+ */
+void *malloc_checked(unsigned int b)
+{   void *ptr = malloc(b);
+	
+ if (ptr == NULL)
+ 	exit(98);
+ 
+ return (ptr);
+}
