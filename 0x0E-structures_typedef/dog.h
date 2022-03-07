e39d3e45c680d0b69970c7bd38be1384cb9082e3
khawladaouay <daouaykhawla782@gmail.com>
2022-03-07 11:16:22 +0100

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 /**
- * struct dog - Short description
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
+ * struct dog - doggy struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ * Description: This struct has the data of my_dog
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} DOG;
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
