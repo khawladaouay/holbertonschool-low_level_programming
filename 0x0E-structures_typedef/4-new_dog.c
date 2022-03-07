0696a6e60e359df68598c80d45a38e9566a9f5da
khawladaouay <daouaykhawla782@gmail.com>
2022-03-07 20:02:37 +0100

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 /**
- * new_dog - initializes doggo structure
- * @name: dog name
- * @age: dog age
- * @owner: owner of dog
- * Return: Void
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *d;
-d = malloc(sizeof(dog_t));
-if (d == NULL)
-return (NULL);
-d->name = name;
-d->age = age;
-d->owner = owner;
-return (d);
+	dog_t *dog;
+	int name1, owner1;
+
+	name1 = strlen(name);
+	owner1 = strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (name1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (owner1 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
