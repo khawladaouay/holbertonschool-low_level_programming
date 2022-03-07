e5ee32ff3d452dea0b8e39bdaeb106bd9becd2af
khawladaouay <daouaykhawla782@gmail.com>
2022-03-07 22:03:04 +0100

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from
+ * @void: input
+ * Return: none
+ */
+int main(void)
+{
+	printf("%d\n", __FILE__);
+	return (0);
+}
