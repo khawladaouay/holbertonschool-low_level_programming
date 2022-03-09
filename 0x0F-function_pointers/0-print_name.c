cf500f341e0d1fe9e447f90f73067f66d4b6093f
khawladaouay <daouaykhawla782@gmail.com>
2022-03-09 10:08:19 +0100

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: name
+ * @f: function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
