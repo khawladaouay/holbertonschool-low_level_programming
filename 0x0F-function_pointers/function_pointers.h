36bae89f3b25cab68a092b796b7b600ee09fdf89
khawladaouay <daouaykhawla782@gmail.com>
2022-03-09 09:53:26 +0100

    function

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct array - array struct
+ * @size: size of the arry
+ * Description: This struct has the data of my_dog
+ */
+struct array
+{
+	unsigned size;
+};
+
+typedef struct array size_t;
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+#endif
