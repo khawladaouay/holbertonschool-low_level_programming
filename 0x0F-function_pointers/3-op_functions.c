bc94635b722f22513ca9eb983e29efed37227126
khawladaouay <daouaykhawla782@gmail.com>
2022-03-09 14:22:12 +0100

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "3-calc.h"
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+/**
+ *  op_add- return the sum
+ * @a: input
+ * @b: input
+ * Return: Void
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ *  op_sub- return the difference
+ * @a: input
+ * @b: input
+ * Return: Void
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ *  op_mul- return the multiplication
+ * @a: input
+ * @b: input
+ * Return: Void
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ *  op_div- return the division
+ * @a: input
+ * @b: input
+ * Return: Void
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ *  op_mod- return the modulo
+ * @a: input
+ * @b: input
+ * Return: Void
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
