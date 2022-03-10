42ca270949bf8ae6fcc993f7d6e3f2420d7b42b9
khawladaouay <daouaykhawla782@gmail.com>
2022-03-10 16:44:44 +0100

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (a != NULL)
 			printf("%s", a);
 		else
-			printf("Nil");
+			printf("(Nil)");
 		if ((separator != NULL) && (i != n - 1))
 			printf("%s", separator);
 	}
