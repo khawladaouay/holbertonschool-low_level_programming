3064cb7b4044fefe35203c259920493ccc3a0fd0
khawladaouay <daouaykhawla782@gmail.com>
2022-03-10 16:40:32 +0100

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,9 +23,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		if ((separator != NULL) && (i != n - 1))
 		{
-			printf("%s", separator);
-			va_end(ap);
+			printf("%s", separator);	
 		}
 	}
 	printf("\n");
+	va_end(ap);
 }
