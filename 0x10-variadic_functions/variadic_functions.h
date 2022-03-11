be240f897c6d4ad95a7c434752c6b1ff7bf05203
khawladaouay <daouaykhawla782@gmail.com>
2022-03-11 14:30:27 +0100

    h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,14 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+void print_string(char *separator, va_list args);
+void print_char(char *separator, va_list args);
+void print_int(char *separator, va_list args);
+void print_float(char *separator, va_list args);
+
+typedef struct printer
+{
+	char *type;
+	void (*f)(char *separator, va_list args);
+} pr_t;
 #endif
