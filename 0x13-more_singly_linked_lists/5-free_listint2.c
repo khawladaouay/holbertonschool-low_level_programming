b58b6dd94022dd294a59db5baf0aa49fe2da81a5
khawladaouay <daouaykhawla782@gmail.com>
2022-03-22 12:31:54 +0100

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (*head != NULL)
-		ptr = *head;
+	if (head == NULL)
+		return;
 
-	while (ptr->next != NULL)
+	while (*head)
 	{
-		*head = ptr->next;
-		free(ptr);
-		ptr = *head;
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
 	}
-	free(*head);
 	*head = NULL;
 }
