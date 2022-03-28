7b365ea64407477a1e8d7cb416bb73af6aa65aef
khawladaouay <daouaykhawla782@gmail.com>
2022-03-28 15:00:18 +0100

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,11 +9,13 @@ int append_text_to_file(const char *filename, char *text_content)
 {int fd, wr, length = strlen(text_content);
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_APPEND);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
 	wr = write(fd, text_content, length);
+	else
+	wr = write(fd, 0, 0);
 	if (wr == -1)
 	return (-1);
 	close(fd);
