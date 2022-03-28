ea0bd419b5f0e8e29f1f72b9a308dd4a4c272343
khawladaouay <daouaykhawla782@gmail.com>
2022-03-28 13:44:20 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, length = strlen(text_content) - 1;
+	int fd, wr, length = strlen(text_content);
 
 	if (filename == NULL)
 	return (-1);
