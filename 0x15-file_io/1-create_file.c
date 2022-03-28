fa03b40fd855d6edd30bd45c5b0a61a7e61b2517
khawladaouay <daouaykhawla782@gmail.com>
2022-03-28 15:07:27 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, length = strlen(text_content);
+	int fd, wr;
 
 	if (filename == NULL)
 	return (-1);
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 	return (-1);
 	if (text_content != NULL)
-	wr = write(fd, text_content, length);
+	wr = write(fd, text_content, strlen(text_content));
 	else
 	wr = write(fd, 0, 0);
 	if (wr == -1)
