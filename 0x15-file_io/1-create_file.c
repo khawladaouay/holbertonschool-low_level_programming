568b17d154540fdca61b66d82c400a48bab2d5d4
khawladaouay <daouaykhawla782@gmail.com>
2022-03-28 13:41:45 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	return (-1);
-	fd = open(filename, O_RDONLY | O_WRONLY | O_TRUNC);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
 
 	if (fd == -1)
 	return (-1);
