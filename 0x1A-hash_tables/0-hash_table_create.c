589b37724dae6286c09b81661d2b20fb7c3b8f0f
khawladaouay <daouaykhawla782@gmail.com>
2022-05-27 12:18:10 +0100

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i;
 hash_table_t *table = malloc(sizeof(hash_table_t));
-if(table == NULL)
+if (table == NULL)
 	return (NULL);
 table->size = size;
-table->array = malloc(sizeof(hash_node_t*) * size);
-if(table->array == NULL)
+table->array = malloc(sizeof(hash_node_t *) * size);
+if (table->array == NULL)
 {
 	free(table);
 	return (NULL);
