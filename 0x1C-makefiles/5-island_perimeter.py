93adbd997f033f4bb8f30d0e51ca998501db2580
khawladaouay <daouaykhawla782@gmail.com>
2022-07-14 20:18:19 +0100

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""5-island_perimeter module"""
+"""Module that calculates the perimeter of an island in a grid."""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
+    """Returns the perimeter of the island in grid."""
 
-    perimeter = 0
+    perim = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
                 if i <= 0 or not grid[i - 1][j]:
-                    perimeter += 1
-                if i >= len(grid[i]) - 1 or not grid[i + 1][j]:
-                    perimeter += 1
-                if j <= 0 or grid[i][j - 1]:
-                    perimeter += 1
+                    perim += 1
+                if j <= 0 or not grid[i][j - 1]:
+                    perim += 1
                 if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-                    perimeter += 1
-    return perimeter
+                    perim += 1
+                if i >= len(grid) - 1 or not grid[i + 1][j]:
+                    perim += 1
+
+    return perim
