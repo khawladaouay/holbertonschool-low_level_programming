2220c68ef4012989842ee99503cd2954459b8e40
khawladaouay <daouaykhawla782@gmail.com>
2022-08-30 23:56:01 +0100

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - function that searches for a value in an array
+ * of integers using the binary search algorithm
+ *@array: input
+ *@size: input
+ *@value: input
+ * Return: int
+ */
+int binary_search(int *array, size_t size, int value)
+{
+size_t l = 0, r = size - 1;
+	size_t m, i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (l <= r)
+	{
+		printf("Searching in array: ");
+		for (i = l; i < r ; i++)
+		{
+			printf("%i, ", array[i]);
+		}
+		printf("%i\n", array[r]);
+		m = (l + r) / 2;
+		if (array[m] < value)
+		{
+			l = m + 1;
+		}
+		else if (array[m] > value)
+		{
+			r = m - 1;
+		}
+		else
+		{
+			return (m);
+		}
+	}
+	return (-1);
+}
